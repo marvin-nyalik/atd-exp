0bfd141887e1dbc9ba243aeb7438aeebd2fc2036
MARVIN <o.silverygold@gmail.com>
2024-07-03 12:10:15 +0300

    Organize middleware, constants in utils

---
+++ src/utils/constants.mjs
@@ -0,0 +1,10 @@
+export const mock_users = [
+  { id: 1, username: "Marv", name: "Marvin" },
+  { id: 2, username: "Aleagy", name: "Aleagy" },
+  { id: 3, username: "Jane", name: "Jenito" },
+  { id: 4, username: "Lilian", name: "Mutinda" },
+  { id: 5, username: "Jacob", name: "Okoth" },
+  { id: 6, username: "Jazmine", name: "Mutiso" },
+  { id: 7, username: "Cecile", name: "Aketch" },
+  { id: 8, username: "Anthony", name: "Ochieng" },
+];
---
+++ src/utils/middlewares.mjs
@@ -0,0 +1,12 @@
+import { mock_users } from "./constants.mjs";
+
+export const resolveUserMiddleware = (req, res, next) => {
+  const parsedId = parseInt(req.params.id);
+
+  if (isNaN(parsedId)) return res.sendStatus(400);
+
+  const userIdx = mock_users.findIndex((user) => user.id === parsedId);
+  if (userIdx === -1) return res.sendStatus(404);
+  req.userIdx = userIdx;
+  next();
+};
