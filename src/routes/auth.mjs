8af23c3a6b38fb58e266db4958155bb3d94a86c7
MARVIN <o.silverygold@gmail.com>
2024-07-05 00:26:31 +0300

    Add authentication route

---
+++ src/routes/auth.mjs
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import passpt from "../strategies/localStrategy.mjs";
+
+const router = Router();
+
+router.post("/real/auth", passpt.authenticate("local"), (req, res) => {
+  res.status(200).json({ msg: "Authenticated" });
+});
+
+router.post("/logout", (req, res) => {
+  if (!req.user) return res.status(401).json({ msg: "You didn't log in" });
+  req.logout((err) => {
+    if (err) {
+      return res.status(400).json({ msg: "An error occurred" });
+    }
+    req.session.destroy((err) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ msg: "An error occurred while destroying the session" });
+      }
+      res.status(200).json({ msg: "Logged out successfully" });
+    });
+  });
+});
+
+router.get("/status", (req, res, next) => {
+  if (req.user) {
+    console.log(req.user);
+    res.status(200).json({ msg: "You logged in" });
+  } else {
+    console.log("No authenticated user");
+    res.status(400).json({ msg: "You didnt log in" });
+  }
+});
+
+export default router;
