837c8e11ca839274e80522a3e3ce4006ade45f32
MARVIN <o.silverygold@gmail.com>
2024-07-04 11:53:15 +0300

    Add cookie options

--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,26 @@
 import express from "express";
-import baseRouter from './routes/index.mjs';
+import baseRouter from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser('ubungen'));
+app.use(cookieParser("ubungen"));
 app.use(baseRouter);
 
 const PORT = process.env.PORT || 3100;
 
-app.get('/', (req, res) => {
-  res.cookie('greeting', 'guten mogen', { maxAge: 60000, signed: true });
-  res.status(200).json({ msg: 'Hello World'});
-})
+app.get("/", (req, res) => {
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  res.cookie("greeting", "guten mogen", {
+    maxAge: 60000,
+    signed: true,
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: 'lax'
+  });
+  res.status(200).json({ msg: "Hello World" });
+});
 
 app.listen(PORT, () => {
   console.log(`We are running on ${PORT}`);
