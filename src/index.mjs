978561ab358a3bb7ee418c863bde0d04385901e3
MARVIN <o.silverygold@gmail.com>
2024-07-04 11:28:20 +0300

    Demonstrate cookies usage

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,18 @@
 import express from "express";
 import baseRouter from './routes/index.mjs';
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser('ubungen'));
 app.use(baseRouter);
 
 const PORT = process.env.PORT || 3100;
+
+app.get('/', (req, res) => {
+  res.cookie('greeting', 'guten mogen', { maxAge: 60000, signed: true });
+  res.status(200).json({ msg: 'Hello World'});
+})
 
 app.listen(PORT, () => {
   console.log(`We are running on ${PORT}`);
