34cbad3adc83e7a6a4b5e22d05ea978f26ff9fc9
MARVIN <o.silverygold@gmail.com>
2024-07-09 22:09:37 +0300

    Use database to register users

--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -3,6 +3,7 @@ import { mock_users } from "../utils/constants.mjs";
 import { resolveUserMiddleware } from "../utils/middlewares.mjs";
 import { checkSchema, validationResult, matchedData } from "express-validator";
 import { userValidationSchema } from "../utils/validationSchemas.mjs";
+import { User } from "../mongoose/schemas/user.mjs";
 
 const router = Router();
 
@@ -15,8 +16,8 @@ router.post("/api/auth", (req, res) => {
     return res.status(400).json({ msg: "Wrong credentials" });
   req.session.user = user;
   req.sessionStore.get(req.sessionID, (err, data) => {
-    console.log(req.sessionID)
-  })
+    console.log(req.sessionID);
+  });
   res.status(200).json({ user: user });
 });
 
@@ -50,15 +51,24 @@ router.patch("/api/users/:id", resolveUserMiddleware, (req, res) => {
   res.status(200).json(mock_users[userIdx]);
 });
 
-router.post("/api/users", checkSchema(userValidationSchema), (req, res) => {
-  const result = validationResult(req);
-  const data = matchedData(req);
-  if (!result.isEmpty())
-    return res.status(400).json({ errors: result.array() });
-  const newUser = { id: mock_users.length + 1, ...data };
-  mock_users.push(newUser);
-  res.status(201).json(newUser);
-});
+router.post(
+  "/api/users",
+  checkSchema(userValidationSchema),
+  async (req, res) => {
+    const result = validationResult(req);
+    const data = matchedData(req);
+    if (!result.isEmpty())
+      return res.status(400).json({ errors: result.array() });
+    try {
+      const newUser = new User(data);
+      const savedUser = await newUser.save();
+      return res.status(201).json(savedUser);
+    } catch (err) {
+      console.log(err);
+      return res.sendStatus(400);
+    }
+  }
+);
 
 router.delete("/api/users/:id", resolveUserMiddleware, (req, res) => {
   const { userIdx } = req;
