ea0ee44951a5627011291343f680e22f9de68b4d
MARVIN <o.silverygold@gmail.com>
2024-07-11 16:56:48 +0300

    use googleAuth route

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -2,9 +2,11 @@ import { Router } from "express";
 import usersRouter from './users.mjs';
 import productsRouter from './products.mjs';
 import authRouter from './auth.mjs';
+import googleAuthRouter from './google_auth.mjs';
 
 const router = Router();
 
+router.use(googleAuthRouter);
 router.use(usersRouter);
 router.use(productsRouter);
 router.use(authRouter);
