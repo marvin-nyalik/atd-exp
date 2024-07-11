8e301dcf09d77a6f29a815f6291d06ff6316f007
MARVIN <o.silverygold@gmail.com>
2024-07-11 16:55:58 +0300

    Add a schema for google user

---
+++ src/mongoose/schemas/googleUser.mjs
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: true,
+  },
+  googleID: mongoose.Schema.Types.String,
+});
+
+export const GoogleUser = mongoose.model('GoogleUser', UserSchema);
