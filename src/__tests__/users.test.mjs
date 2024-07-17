0facce16bd9b6f0e41421ddca7dd62f17f76dfe4
MARVIN <o.silverygold@gmail.com>
2024-07-17 14:10:29 +0300

    Add test for login, connect.sid cookie set

--- src/__tests__/users.test.mjs
+++ src/__tests__/users.test.mjs
@@ -10,6 +10,11 @@ const user = {
   email: "jane@test.com",
 };
 
+const user_to_auth = {
+  username: "JenitoTechie",
+  password: "Jenito1234",
+};
+
 const invalid_user = {
   password: "Jenito1234",
   email: "jane@test.com",
@@ -17,6 +22,7 @@ const invalid_user = {
 
 describe("User Registration", () => {
   let app;
+  
   beforeAll(async () => {
     connectDB(process.env.MONGODB_TEST_URI)
       .then(() => {
@@ -30,8 +36,9 @@ describe("User Registration", () => {
       console.log(`Testing on port 3100`);
     });
   });
+
   beforeEach(async () => {
-    await User.deleteMany({});
+    console.log(`--------------------------------------------------`);
   });
 
   it("Should register a User with valid data", async () => {
@@ -43,6 +50,15 @@ describe("User Registration", () => {
     const res = await request(app).post("/api/users").send(invalid_user);
     expect(res.status).toBe(400);
   });
+
+  it("Should login a valid user", async () => {
+    const res = await request(app).post("/real/auth").send(user_to_auth);
+    expect(res.status).toBe(200);
+    
+    const cookies = res.headers['set-cookie'];
+    expect(cookies).toBeDefined();
+    const connectSidCookie = cookies.find(cookie => cookie.startsWith('connect.sid='));
+    expect(connectSidCookie).toBeDefined();  });
 
   afterAll(async () => {
     await mongoose.connection.dropDatabase();
