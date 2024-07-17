2e0a8a2f049acb177fe36578cc28b74648eb80b8
MARVIN <o.silverygold@gmail.com>
2024-07-17 13:39:22 +0300

    Extract create express app logic

---
+++ createExpressApp.mjs
@@ -0,0 +1,55 @@
+import express from "express";
+import baseRouter from './src/routes/index.mjs';
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+import MongoStore from "connect-mongo";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const createApp = () => {
+  const app = express();
+  dotenv.config();
+  app.use(express.json());
+  app.use(cookieParser("ubungen"));
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || "sessions-key",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 60000 * 10,
+      },
+      store: MongoStore.create({
+        client: mongoose.connection.getClient(),
+      }),
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // use base router
+  app.use(baseRouter);
+  const isTest = process.env.IS_TEST
+
+  // CREATING A COOKIE EXAMPLE
+  app.get("/", (req, res) => {
+    const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("greeting", "guten mogen", {
+      maxAge: 60000,
+      signed: true,
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: "lax",
+    });
+    res.status(200).json({ msg: "Hello World" });
+  });
+
+  return app;
+};
+
+export default createApp;
