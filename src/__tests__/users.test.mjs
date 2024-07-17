e6cf351318e4567bc43fe3e4bf8e5405b7524d14
MARVIN <o.silverygold@gmail.com>
2024-07-17 13:40:07 +0300

    fix bug: update app in test definition

--- src/__tests__/users.test.mjs
+++ src/__tests__/users.test.mjs
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import { User } from "../mongoose/schemas/user.mjs";
-import app from "../index.mjs";
 import request from "supertest";
 import connectDB from "../../db.mjs";
+import createApp from "../../createExpressApp.mjs";
 
 const user = {
   username: "JenitoTechie",
@@ -10,24 +10,42 @@ const user = {
   email: "jane@test.com",
 };
 
-// beforeAll(async () => {
-//   await connectDB(`mongodb://127.0.0.1:27017/express-test-adt`).then(() => {
-//     console.log(`Connected for test`);
-//   });
-// });
-
-afterAll(async () => {
-  await mongoose.connection.dropDatabase();
-  await mongoose.connection.close();
-});
-
-beforeEach(async () => {
-  await User.deleteMany({});
-});
+const invalid_user = {
+  password: "Jenito1234",
+  email: "jane@test.com",
+};
 
 describe("User Registration", () => {
+  let app;
+  beforeAll(async () => {
+    connectDB(process.env.MONGODB_TEST_URI)
+      .then(() => {
+        console.log(`Connected to test mongo db`);
+      })
+      .catch((err) => console.log(err));
+
+    app = createApp();
+
+    app.listen(3100, () => {
+      console.log(`Testing on port 3100`);
+    });
+  });
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+
   it("Should register a User with valid data", async () => {
     const res = await request(app).post("/api/users").send(user);
     expect(res.status).toBe(201);
+  });
+
+  it("Should not register an invalid User", async () => {
+    const res = await request(app).post("/api/users").send(invalid_user);
+    expect(res.status).toBe(400);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
   });
 });
